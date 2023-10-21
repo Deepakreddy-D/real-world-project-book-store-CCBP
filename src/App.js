e17a6cc57189ed702e7bdd8e328d6de81b261cde
= <=>
2023-10-21 14:59:52 +0530

    Add links to the nav items in Header and Fix navigation issue
    To fix the issue with navigation the BrowserRouter was nested inside the React StrictMode in index.js

--- src/App.js
+++ src/App.js
@@ -14,17 +14,15 @@ import './App.css';
 class App extends Component {
   render(){
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path ="/" component = {Home}/>
-          <Route exact path ="/books" component = {BookList}/>
-          <Route exact path ="/books/:id" component = {BookDetails}/>
-          <Route exact path ="/cart" component = {Cart}/>
-          <Route exact path ="/checkout" component = {Checkout}/>
-          <Route exact path ="/not-found" component = {NotFound}/>
-          <Redirect to = "/not-found"/>
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path ="/" component = {Home}/>
+        <Route exact path ="/books" component = {BookList}/>
+        <Route exact path ="/books/:id" component = {BookDetails}/>
+        <Route exact path ="/cart" component = {Cart}/>
+        <Route exact path ="/checkout" component = {Checkout}/>
+        <Route exact path ="/not-found" component = {NotFound}/>
+        <Redirect to = "/not-found"/>
+      </Switch>
     );
   }
 }
