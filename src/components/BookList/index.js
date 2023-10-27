5a19d84aca16e8c25fd379c786ee94ecbaabde32
= <=>
2023-10-27 20:29:44 +0530

    Add 'new' API call to get list of books

--- src/components/BookList/index.js
+++ src/components/BookList/index.js
@@ -1,13 +1,13 @@
-import {Component} from "react"
+import { Component } from "react";
 
-import Header from "../Header"
+import Header from "../Header";
 import SearchInput from "../SearchInput";
 import PriceRange from "../PriceRange";
 import BookItem from "../BookItem";
-import Loader from "../Loader"
+import Loader from "../Loader";
 import ErrorMessage from "../ErrorMessage";
 
-import "./index.css"
+import "./index.css";
 
 // const response = {
 //     "total": "20",
@@ -39,31 +39,87 @@ import "./index.css"
 //     ]
 // }
 
-class BookList extends Component {
-    render(){
-        return (
-            <>
-                <Header/>                
-                <div className = "book-list-container">
-                    <div className = "book-list-content-container">
-                        <SearchInput/>
-                        <h1 className="book-items-heading">Books</h1>
-                        <PriceRange/>                        
-                        <div className = "book-list-container">
-                            <BookItem/>
-                            <BookItem/>
-                            <BookItem/>
-                            <BookItem/>
-                            <BookItem/>
-                            <BookItem/>
-                        </div>
-                    </div>                  
-                </div>
-                <Loader/>  
-                <ErrorMessage/>
-            </> 
-        );
-    }
+const apiStatusConstants ={
+    initial: "INITIAL",
+    inProgress: "IN_PROGRESS",
+    success: "SUCCESS",
+    failure: "FAILURE"
 }
 
-export default BookList
+const book_list_url = "https://api.itbook.store/1.0/new";
+
+class BookList extends Component {
+    state = {
+        apiStatus: apiStatusConstants.initial,
+        booksData: []
+    } 
+
+  componentDidMount() {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+    this.getBooks();
+  }
+
+  getBooks = async () => {
+    const response = await fetch(book_list_url)
+    if (response.ok) {
+        const jsonResponse = await response.json();
+        this.setState({apiStatus: apiStatusConstants.success, booksData: jsonResponse.books})
+        console.log(jsonResponse.books)
+    } else if (response.status === 404) {
+        this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  };
+
+  renderLoadingView(){
+    return <Loader />
+  }
+
+  renderSuccessView(){
+    const {booksData} = this.state
+    return (
+        <>
+            <h1 className="book-items-heading">Books</h1>
+            <PriceRange />
+            <div className="book-list-container">
+            {booksData.map((eachbook) => (
+                <BookItem bookItemDetails={eachbook}/>
+            ))}                
+            </div>
+        </>
+    )
+  }
+
+  renderFailureView(){
+    return  <ErrorMessage />
+  }
+
+  renderResults(){
+    const {apiStatus} = this.state
+    switch(apiStatus) {
+        case apiStatusConstants.inProgress:
+            return this.renderLoadingView();
+        case apiStatusConstants.success:
+            return this.renderSuccessView();
+        default:
+            return this.renderFailureView();
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="book-list-container">
+          <div className="book-list-content-container">
+            <SearchInput />
+            {this.renderResults()}
+          </div>
+        </div>
+        
+
+      </>
+    );
+  }
+}
+
+export default BookList;
